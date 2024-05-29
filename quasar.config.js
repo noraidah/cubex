128ce5d2d0c773a43f191d0f8dc606f5f51c8fef
Omar <38073753+lordsayur@users.noreply.github.com>
2024-05-29 12:09:10 +0800

    ✨ set vueRouterBase

--- quasar.config.js
+++ quasar.config.js
@@ -45,7 +45,7 @@ module.exports = configure(function (/* ctx */) {
       },
 
       vueRouterMode: "history", // available values: 'hash', 'history'
-      // vueRouterBase,
+      vueRouterBase: "/cubex/",
       // vueDevtools,
       // vueOptionsAPI: false,
 
