a09a6ea03109852b7c0367ae6e27a1e8152c484a
Omar <38073753+lordsayur@users.noreply.github.com>
2024-05-29 12:12:34 +0800

    ✨ set publicPath

--- quasar.config.js
+++ quasar.config.js
@@ -51,7 +51,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: "/cubex/",
       // analyze: true,
       // env: {},
       // rawDefine: {}
